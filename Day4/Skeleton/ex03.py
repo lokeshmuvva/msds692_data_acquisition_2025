a3302da5d3000db2be6a560789a34ab51330d8a0
lokeshmuvva <lmuvva@gmail.com>
2025-09-08 14:37:16 -0700

    Working on Day 4 Example 2

--- Day4/Skeleton/ex03.py
+++ Day4/Skeleton/ex03.py
@@ -1,3 +1,6 @@
+"""
+Module for fetching SF police data and storing it to Google Cloud Storage.
+"""
 import datetime
 import json
 import os
@@ -8,13 +11,23 @@ from google.oauth2 import service_account
 from google.cloud import storage
 
 
-def store_to_gcs(service_account_key: str,
+def store_to_gcs(service_account_key_path: str,
                  project_id: str,
                  bucket_name: str,
                  file_name: str,
                  data: str) -> None:
+    """
+    Store data to Google Cloud Storage.
+
+    Args:
+        service_account_key_path: Path to service account key file
+        project_id: GCP project ID
+        bucket_name: GCS bucket name
+        file_name: Name of file to store
+        data: Data to store as string
+    """
     credentials = service_account.Credentials.from_service_account_file(
-        service_account_key)
+        service_account_key_path)
     client = storage.Client(project=project_id,
                             credentials=credentials)
     bucket = client.bucket(bucket_name)
@@ -22,25 +35,37 @@ def store_to_gcs(service_account_key: str,
     file.upload_from_string(data)
 
 
-def get_json_response(url: str, api_key: str):
+def get_json_response(api_url: str, api_key: str):
+    """
+    Get JSON response from API endpoint.
+
+    Args:
+        api_url: URL to fetch data from
+        api_key: API key for authentication
+
+    Returns:
+        JSON response from API
+    """
     header = {'X-Api-Key': api_key}
-    response = requests.get(url, headers=header)
+    response = requests.get(api_url, headers=header, timeout=30)
     return response.json()
 
 
 if __name__ == '__main__':
     load_dotenv(
-        dotenv_path='/Users/dwoodbridge/Class/2025_MSDS692/Example/Example/.env')
+        dotenv_path='/Users/lokeshmuvva/'
+        'Documents/msds692_data_acquisition_2025/Day3/.env'
+    )
     data_gov_api_key = os.getenv("DATA_GOV_API_KEY")
     genai_api_key = os.getenv("GCP_GENAI_API_KEY")
     service_account_key = os.getenv("GCP_SERVICE_ACCOUNT_KEY")
     project_id = os.getenv("GCP_PROJECT_ID")
     bucket_name = os.getenv("GCP_BUCKET_NAME")
-    file_name = f"sf_police_report/{datetime.date.today()}.json"
-    url = 'https://data.sfgov.org/resource/wg3w-h783'
-    data = get_json_response(url, data_gov_api_key)
+    FILE_NAME = f"sf_police_report/{datetime.date.today()}.json"
+    URL = 'https://data.sfgov.org/resource/wg3w-h783'
+    data = get_json_response(URL, data_gov_api_key)
     store_to_gcs(service_account_key,
                  project_id,
                  bucket_name,
-                 file_name,
+                 FILE_NAME,
                  json.dumps(data, indent=4))
